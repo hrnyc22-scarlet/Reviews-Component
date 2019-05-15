7848205e40c3f871b1a05e414543241c6d8ddfef
bengold14 <bengold14@gmail.com>
2019-05-15 15:12:31 -0400

    connected Sqlitconnected sqlite3 databases and inserted Data

---
+++ client/src/app.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+class App extends React.Component {
+  constructor (props) {
+    super (props)
+    this.state = {
+
+    }
+  }
+
+  render () {
+
+  }
+ }
+
+ ReactDOM.render(<App/>,document.findElementById('app'))
