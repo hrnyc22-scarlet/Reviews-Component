8c560c7f58e85163bf83856f016e9056a5b63dd2
bengold14 <bengold14@gmail.com>
2019-05-21 13:09:02 -0400

    completed search component styling

--- client/src/components/searchComponent/search.jsx
+++ client/src/components/searchComponent/search.jsx
@@ -4,7 +4,8 @@ const search = function (props) {
   return (
     <form className="search-container">
       <img src="./Search Icon.png" className="search-icon"></img>
-      <input type="text" name="name" className="input"/>
+      <input value="Search reviews" type="text" name="name" className="input"/>
+      <img src="./Cancel Search Icon.png" className="cancel-search-icon"></img>
     </form>
   )
 }
